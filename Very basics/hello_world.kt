c119fad79942d746318a9425baa228521cbad8d1
Karan Bhatia <karanbhatia2050@gmail.com>
2018-09-18 19:18:43 +0530

    Create hello_world.kt

---
+++ Very basics/hello_world.kt
@@ -0,0 +1,18 @@
+/*
+This is a sample Hello world Program
+*/
+
+fun main(args: Array<String>)
+{
+    println("Hello World")
+}
+
+/*
+fun -> the way of defining a Function in kotlin
+
+args: Array<String> -> defines a array variable args of type String
+
+println("Hello world"); -> prints Hello world! onm the standard output stream followed by a newline
+
+Note too the lack of any semicolons ";" anywhere!
+*/
