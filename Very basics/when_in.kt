76705a8c61bca6e88a992022c8b01ed9f21f8c86
Karan Bhatia <karanbhatia2050@gmail.com>
2018-09-27 19:26:12 +0530

    Update when_in.kt

--- Very basics/when_in.kt
+++ Very basics/when_in.kt
@@ -1,16 +1,14 @@
-//Program to check whether a given letter is uppercase or lowercase using 
+//Program to check whether a given letter is uppercase or lowercase using
 // when operator and in operator
 
-import java.util.Scanner
 fun main(args: Array<String>)
 {
-    val reader = Scanner(System.`in`)
-    println("Enter the letter (use \" \" to escape)\n")
+    println("Enter the letter (use 0 to escape)\n")
     do {
         var a = readLine()!!.single() //Note that this is the proper way to read a Character in Kotlin
         when (a) {
             in 'A'..'Z' -> println("The letter is Uppercase\n")
-            in 'a'..'z' -> println("The letter is lowercase\n")
+            in 'a'..'z' -> println("The letter is lowercase\n")  //Did you notice the missing else? I thought that was mandatory...
         }
-    }while(a in 'a'..'z' || a in 'A'..'Z')
+    }while(a in 'a'..'z' || a in 'A'..'Z') //the .. operator is called Range operator and returns true if a is in the range a - z or A - Z
 }
