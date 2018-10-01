f93bbb2bfe58229990464ef9d9a80d1a591b26cf
Karan Bhatia <karanbhatia2050@gmail.com>
2018-10-01 14:39:55 +0530

    Create types_of_equality.kt

---
+++ basics/types_of_equality.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var a = Integer(5)
+    var b = Integer(5)    
+    
+    //Structural equality test
+    if(a==b) println("a is structurally equal to b\n")
+    else println("a is NOT structurally equal to b\n")
+    
+    //Referential equality test
+    if(a===b) println("a is referentially equal to b\n")
+    else println("a is NOT referentially equal to b\n")
+}
